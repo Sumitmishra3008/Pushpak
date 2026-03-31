9dc511efbd85ab75d8cae886e92622b196a6d1b0
Sumitmishra3008 <submit.mishra2002@gmail.com>
2026-04-01 00:53:40 +0530

    captain home

--- frontend/src/components/VehiclePanel.jsx
+++ frontend/src/components/VehiclePanel.jsx
@@ -47,7 +47,7 @@ const VehiclePanel = (props) => {
       >
         <img
           className="h-10"
-          src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
+          src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=552/height=552/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy85NTM4NTEyZC1mZGUxLTRmNzMtYmQ1MS05Y2VmZjRlMjU0ZjEucG5n"
           alt=""
         />
         <div className="-ml-2 w-1/2">
@@ -73,7 +73,7 @@ const VehiclePanel = (props) => {
       >
         <img
           className="h-10"
-          src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+          src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=552/height=552/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy9mYzEwMWZmOC04MWExLTQ2YzMtOTk1YS02N2I0YmJkMmYyYmYuanBn"
           alt=""
         />
         <div className="ml-2 w-1/2">
